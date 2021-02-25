48abfaf5135ea1b8edfe37ffc2d2c3e448c57185
MLujanIG <77990183+MLujanIG@users.noreply.github.com>
2021-02-25 13:24:48 -0700

    Prototipo en Unity, SwipeMenu and DialogueManager Scenes

---
+++ Prototype/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue 
+{
+    public string name;
+    [TextArea(3, 10)]
+    public string[] sentences;
+
+    public void StartDialogue (Dialogue dialogue)
+    {
+        Debug.Log ("Starting conversation with " + dialogue.name);
+    }
+}
